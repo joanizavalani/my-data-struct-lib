7e0285f455c0638fbca2c797ac26be6ebe83bf21
Joani Zavalani <joanizavalani@gmail.com>
2025-03-26 09:43:29 +0100

    added a couple of new methods on my LinkedList class; most notably joinList()

--- src/linkedlist/singly/Demo.java
+++ src/linkedlist/singly/Demo.java
@@ -4,12 +4,23 @@ public class Demo {
 
     public static void main(String[] args) {
         LinkedList<Integer> l1 = new LinkedList<>();
-        ListIterator<Integer> itr1 = new ListIterator<>(l1.getHeader());
-
-        l1.add(1, itr1);
-        l1.add(2, itr1);
-        l1.add(3, itr1);
-        l1.add(4, itr1);
+        l1.add(-1);
+        l1.add(1);
+        l1.add(3);
+        l1.add(5);
+        l1.add(6);
+        l1.add(7);
         l1.printList();
+
+        LinkedList<Integer> l2 = new LinkedList<>();
+        l2.add(-2);
+        l2.add(0);
+        l2.add(5);
+        l2.add(10);
+        l2.add(12);
+        l2.printList();
+
+        LinkedList<Integer> l3 = l1.joinList(l2);
+        l3.printList();
     }
 }
--- src/linkedlist/singly/LinkedList.java
+++ src/linkedlist/singly/LinkedList.java
@@ -1,13 +1,13 @@
 package linkedlist.singly;
 
-public class LinkedList<T> {
+public class LinkedList<T extends Comparable<T>> {
 
     // A singly-linked list has a header, which for the programmer on the other end is hidden from him,
     // which is why it has a null value. The first element (index = 0) of the list is the node that
     // comes after the header. This is why the constructors automatically assign the header's value to null.
     // This makes processes such as addition before the first "usable" node possible.
 
-    private final ListNode<T> header;
+    private ListNode<T> header;
 
     public LinkedList() {
 
@@ -26,9 +26,80 @@ public class LinkedList<T> {
         }
     }
 
+    public void add(T value) {
+
+        ListNode<T> node = header;
+
+        while (node.getNext() != null)
+            node = node.getNext();
+
+        node.setNext(new ListNode<>(value, null));
+    }
+
+    public void remove(T value) {
+
+        ListNode<T> node = header.getNext();
+
+        while (node != null && !node.getNext().getValue().equals(value))
+            node = node.getNext();
+
+        node.setNext(node.getNext().getNext());
+    }
+
+    public int size() {
+
+        int count = 0;
+        ListNode<T> node = header;
+
+        while (node.getNext() != null) {
+            count++;
+            node = node.getNext();
+        }
+        return count;
+    }
+
     public boolean isEmpty() {
 
         return header.getNext() == null;
+    }
+
+    public void makeEmpty() {
+
+        header.setNext(null);
+    }
+
+    public LinkedList<T> joinList(LinkedList<T> list2) {
+
+        ListNode<T> node1 = header.getNext();
+        ListNode<T> node2 = list2.header.getNext();
+
+        if (isEmpty() || list2.isEmpty()) {
+            return null;
+        }
+
+        LinkedList<T> list3 = new LinkedList<>();
+
+        while (node1 != null && node2 != null) {
+
+            if (node1.getValue().compareTo(node2.getValue()) < 0) {
+                list3.add(node1.getValue());
+                node1 = node1.getNext();
+            } else {
+                list3.add(node2.getValue());
+                node2 = node2.getNext();
+            }
+        }
+
+        while (node1 != null) {
+            list3.add(node1.getValue());
+            node1 = node1.getNext();
+        }
+
+        while (node2 != null) {
+            list3.add(node2.getValue());
+            node2 = node2.getNext();
+        }
+        return list3;
     }
 
     public void printList() {
